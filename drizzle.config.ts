94880324f06ab839ac76fbe28ebe354ec4710fde
thefinalb055 <finalb055@skiff.com>
2024-06-13 20:04:10 +0700

    chore: :construction_worker:

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,10 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/lib/env.mjs";
 
 export default {
+  dialect: "postgresql",
   schema: "./src/lib/db/schema",
   out: "./src/lib/db/migrations",
-  driver: "pg",
   dbCredentials: {
-    connectionString: env.DATABASE_URL.concat("?sslmode=require"),
-  }
+    url: env.DATABASE_URL.concat("?sslmode=require"),
+  },
 } satisfies Config;
